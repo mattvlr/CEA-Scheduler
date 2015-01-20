5b261d2e2ded9b2f6e921286b81786daff4f5830
mattvlr <mattv.vandenberg@gmail.com>
2015-01-19 23:30:09 -0600

    Started work on main site

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+//require_once('login.php'); <- uncomment to force a login page redirect
+require_once('sidebar.php');
+
+
+?>
+<html>
+<head>
+    <!-- Bootstrap core CSS -->
+    <link href="/resources/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/resources/css/dashboard.css" rel="stylesheet">
+</head>
+
+</html>
---
+++ sidebar.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+
+	$sidebar = '<div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+		  <li class="list-group-item">Schedule</li>
+		  <li class="list-group-item">Application Settings</li>
+		  <li class="list-group-item">Admin Section</li>
+		  <li class="list-group-item">Driver Home Page</li>
+		  <li class="list-group-item">Student Home Page</li>
+		  </ul>
+		
+		  </div>';
+
+
+echo $sidebar;
+?>
