15f9c661e1bb0c36619d0ae31fdb7e2723dc4684
mattvlr <mattv.vandenberg@gmail.com>
2015-01-28 12:52:04 -0600

    Commented code

--- mysql/_db.php
+++ mysql/_db.php
@@ -1,5 +1,5 @@
 <?php
-
+//call this file in order to connect to the database, there is an example of this in test.php
 $base_url = 'http://localhost:3306';
 abstract class db_info
 {	
@@ -7,12 +7,12 @@ abstract class db_info
 	// Our MySql Connection information
 	// $base_url - used for link generation
 	////////////////////////////////////////////
-	const DB_SERVER = '104.131.179.153';
-	const DB_PORT = '';
-	const DB_USERNAME = 'web';
-	const DB_PASSWORD = 'cea';
-	const DB_DATABASE = 'Scheduler';
-    const BASE_URL = 'http://104.131.179.153:3306';
+	const DB_SERVER = '104.131.179.153'; //ip of server
+	const DB_PORT = ''; //port is 3306, but we don't need it 
+	const DB_USERNAME = 'web'; //made a username so we know what takes up most resources
+	const DB_PASSWORD = 'cea'; //
+	const DB_DATABASE = 'Scheduler'; // where we keep our info
+    const BASE_URL = 'http://104.131.179.153:3306'; //connection url string
 	
 	public $failed = false;
 	protected $cur_query = "";
--- mysql/test.php
+++ mysql/test.php
@@ -6,30 +6,27 @@ echo "TESTING THE MYSQL DRIVER<BR>";
 
 $mysql = new mysql_driver;
 
-if($mysql->connect())
+if($mysql->connect()) //we we're connected output the echo statements
 {
 echo "MYSQL connected!<br>";
 echo "Selecting User Table<br>";
 }
 
-//$found = $mysql->select("Users","*","Username='*'");
+//$mysql->query takes in any SQL statement and outputs an mysqli array of data
 $found = $mysql->query("Select * from Users");
-//echo "FOUND : " . $found[0] . "<br>";
 
-echo '<br>';
-//$result = $found->fetch_array(MYSQLI_NUM);
 
-if($found->num_rows > 0){
-	while($row = $found->fetch_assoc()){
-	var_dump($row);
-	echo('<br>');
+echo '<br>'; // new line for readability
+
+
+if($found->num_rows > 0){ //if we got something back do this
+
+	while($row = $found->fetch_assoc()){ //prints each row and converts the mysqli datatype to an associative php array.
+		var_dump($row);
+		echo('<br>');
 	}
-	}
+}
 
-
-//$found = $mysql->getSessionInfo($_COOKIE['id']);  // example of loading session info fo user 1 this is used for loading userdata to a session when a cookie is present
-
-//print_r($found);
 
 
 
