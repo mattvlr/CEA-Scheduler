9d2566f4a3fba1941beab357bebe5b3712bdecc9
Matt Vandenberg <mattv.vandenberg@gmail.com>
2015-03-11 20:59:18 -0500

    Added User Details on main.php

--- main.php
+++ main.php
@@ -1,3 +1,16 @@
 <div>
-test
+<?php
+$permission = ''; 
+if($_SESSION['PERMISSION'] == 1)
+	$permission = "Student";
+else if($_SESSION['PERMISSION'] == 2)
+	$permission = "Driver";
+else if($_SESSION['PERMISSION'] == 3)
+	$permission = "Administrator";
+
+var_dump($_SESSION);
+$text ='<br><br><br><p>Your name is <bold><font size="5">'. $_SESSION['FIRST_NAME'] . '</font></bold> <bold><font size="5">'. $_SESSION['LAST_NAME'] . '</font></bold>
+		. Your username is <bold><font size="5">'. $_SESSION['USERNAME'] . '</font></bold> and your permission on this site is <bold><font size="5">'. $permission . '</font></bold></p> ';
+echo $text;
+ ?>
 </div>
--- sidebar.php
+++ sidebar.php
@@ -7,6 +7,7 @@
          <div class="row">
         	 <div class="col-sm-3 col-md-2 sidebar" padding-top:50px>
           	 	 <ul class="nav nav-sidebar">
+          	 	 	 <li><a href="?act=m">Site Home</a></li>
 				 	 <li><a href="?act=admin">Admin Home</a></li>
 				 </ul>
 		  	 </div>
