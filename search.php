a20549864160054def4469d09cace11f329bb8cc
Matt Vandenberg <mattv.vandenberg@gmail.com>
2015-04-19 16:12:15 -0500

    Fixed Search Colors

--- search.php
+++ search.php
@@ -44,7 +44,7 @@ $search_string = $db->real_escape_string($search_string);
 // Check Length More Than One Character
 if (strlen($search_string) >= 1 && $search_string !== ' ') {
 	// Build Query
-	$query = 'SELECT * FROM Users WHERE FIRST_NAME LIKE "%'.$search_string.'%" OR LAST_NAME LIKE "%'.$search_string.'%";';
+	$query = 'SELECT * FROM Users WHERE FIRST_NAME LIKE "%'.$search_string.'%" OR LAST_NAME LIKE "%'.$search_string.'%" ORDER BY PERMISSION DESC;';
 
 	// Do Search
 	$result = $db->query($query);
@@ -55,19 +55,23 @@ if (strlen($search_string) >= 1 && $search_string !== ' ') {
 	// Check If We Have Results
 	if (isset($result_array)) {
 		foreach ($result_array as $result) {
-				//var_dump($result_array);
-			// Format Output Strings And Hightlight Matches
+
 			
 			$first_name = preg_replace("/".$search_string."/i", "<b class='highlight'>".$search_string."</b>", $result['FIRST_NAME']);
 			$last_name = preg_replace("/".$search_string."/i", "<b class='highlight'>".$search_string."</b>", $result['LAST_NAME']);
 
-			// Insert Name
-			//$output = str_replace('firstname', $first_name, $html);
-			//$output = str_replace('lastname', $last_name, $html);
-
-			// Insert Function
-			//$output = str_replace('functionString', $display_function, $output);
-			$output = '<li class="list-group-item"><h4>'.$first_name.' '.$last_name.'</h4></li>';
+			
+			if($result['PERMISSION'] == 3){ //admin
+				$output = '<li class="list-group-item list-group-item-danger"><h4>'.$first_name.' '.$last_name.'<span class="badge" style="float:right">Admin</span></h4></li>';
+			} elseif($result['PERMISSION'] == 2){ //driver
+				$output = '<li class="list-group-item list-group-item-info"><h4>'.$first_name.' '.$last_name.'<span class="badge"  style="float:right">Driver</span></li>';
+			} elseif($result['PERMISSION'] == 1){ //student
+				$output = '<li class="list-group-item list-group-item-success"><h4>'.$first_name.' '.$last_name.'<span class="badge" style="float:right">Student</span></h4></li>';
+			} elseif($result['PERMISSION'] == 0){ //guest
+				$output = '<li class="list-group-item"><h4>'.$first_name.' '.$last_name.'<span class="badge"  style="float:right">Guest</span></h4></li>';
+			} else{ //inactive / everyone else??
+				$output = '<li class="list-group-item list-group-item-warning"><h4>'.$first_name.' '.$last_name.'<span class="badge"  style="float:right">Inactive</span></h4></li>';
+			}
 			// Output
 			echo($output);
 		}
