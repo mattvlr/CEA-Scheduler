28d3c1b15b3a2f1717c5b5c1d520d41a9cbac4a8
Matt Vandenberg <mattv.vandenberg@gmail.com>
2015-04-28 13:07:53 -0500

    schedule changes

--- schedule.php
+++ schedule.php
@@ -22,7 +22,7 @@ $query = 'SELECT * FROM Schedules WHERE DAY = "'. $day . '" ORDER BY PickupTime 
 echo'<div class="panel panel-primary" style="width=90%;margin-right:4%">
 
   <div class="panel-heading"><h3 class="panel-title">Schedule '.$day.'</h3></div>
-  <table class="table"><thread><tr><th>Golf Cart</th><th>Student Name</th><th>Driver</th><th>Pickup Time<th>Pickup Point</th><th>Dropoff Time</th><th>Dropoff Point</th><th>Backup Driver</th></tr></thread>';
+  <table class="table table-striped"><thread><tr><th>Golf Cart</th><th>Student Name</th><th>Driver</th><th>Pickup Time<th>Pickup Point</th><th>Dropoff Time</th><th>Dropoff Point</th><th>Backup Driver</th></tr></thread>';
 	// Do Search
 	$result = $db->query($query);
 	while($results = $result->fetch_array()) {
@@ -32,19 +32,14 @@ echo'<div class="panel panel-primary" style="width=90%;margin-right:4%">
 	// Check If We Have Results
 	if (isset($result_array)) {
 		foreach ($result_array as $result) {
-			//print_r($result);
-			//echo("<br>");
-			//if($result['PERMISSION'] == 3){
-				echo '<tr><td>'.$result['Cart'].'</td><td>'.$result['Student_First']. ' ' . $result['StudentLast'].'</td><td>'.$result['Driver'].'</td><td>'.$result['PickupTime'].'</td><td>'.$result['PickupPoint'].'</td><td>'.$result['DropTime'].'</td><td>'.$result['DropPoint'].'</td><td>'.$result['BackupDriver'].'</td></tr>';
-			//} //elseif($result['PERMISSION'] == 2){
-			//	echo '<tr class="info"  data-href="?act=profile&u='.$result["USERNAME"].'"><td>'.$result['ID'].'</td><td>'.$result['LAST_NAME'].'</td><td>'.$result['FIRST_NAME'].'</td><td>'.$result['UniversityID'].'</td><td>'.$result['USERNAME'].'</td><td>Driver</td></tr>';
-			//} elseif($result['PERMISSION'] == 1){
-			//	echo '<tr class="success"  data-href="?act=profile&u='.$result["USERNAME"].'"><td>'.$result['ID'].'</td><td>'.$result['LAST_NAME'].'</td><td>'.$result['FIRST_NAME'].'</td><td>'.$result['UniversityID'].'</td><td>'.$result['USERNAME'].'</td><td>Student</td></tr>';
-			//} elseif($result['PERMISSION'] == 0){
-			//	echo '<tr  data-href="?act=profile&u='.$result["USERNAME"].'"><td>'.$result['ID'].'</td><td>'.$result['LAST_NAME'].'</td><td>'.$result['FIRST_NAME'].'</td><td>'.$result['UniversityID'].'</td><td>'.$result['USERNAME'].'</td><td>Guest</td></tr>';
-			//} else{
-			//	echo '<tr class="warning"  data-href="?act=profile&u='.$result["USERNAME"].'"><td>'.$result['ID'].'</td><td>'.$result['LAST_NAME'].'</td><td>'.$result['FIRST_NAME'].'</td><td>'.$result['UniversityID'].'</td><td>'.$result['USERNAME'].'</td><td>Inactive</td></tr>';
-			//}
+				if($result['Driver'] == "No Driver"){
+					$warn = 'class="danger"';
+				}
+				if($result['BackupDriver'] == "No Backup" ){
+					$bdwarn = 'class="danger"';
+				}
+				echo '<tr><td>'.$result['Cart'].'</td><td>'.$result['Student_First']. ' ' . $result['StudentLast'].'</td><td '.$warn.'>'.$result['Driver'].'</td><td>'.$result['PickupTime'].'</td><td>'.$result['PickupPoint'].'</td><td>'.$result['DropTime'].'</td><td>'.$result['DropPoint'].'</td><td ' .$bdwarn.'>'.$result['BackupDriver'].'</td></tr>';
+
 			
 		}
 	}
